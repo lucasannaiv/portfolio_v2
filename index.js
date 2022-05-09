41832a092dbd71b5cc5564a045e48c40ff1210e9
Lucas Vianna <zlucasvianna@gmail.com>
2022-05-09 19:31:59 -0300

    including a new header link

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function displayAbout() {
+    let arrow = document.getElementById('animation');
+    arrow.classList.toggle('rotate');
+
+    let display = document.getElementById('about');
+    display.classList.toggle('active');
+    }
+
+
