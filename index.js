801a17771131e307db9406381bccb2cd249dee37
Lucas Vianna <zlucasvianna@gmail.com>
2022-05-16 00:50:40 -0300

    new design

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 
-function displayAbout() {
-    let arrow = document.getElementById('animation');
-    arrow.classList.toggle('rotate');
+    function displayAbout() {
+        let arrow = document.getElementById('animation');
+        arrow.classList.toggle('rotate');
 
-    let display = document.getElementById('about');
-    display.classList.toggle('active');
-    }
+        let display = document.getElementById('about');
+        display.classList.toggle('active');
+        }
+
+
+
+
+    function emailCheck(email) {
+
+        // Check if email is in the right pattern 
+        var result = /\S+@\S+\.\S+/;
+        return result.test(email);
+
+        }
 
